33e2543ee676c3fe86ae7d45299e84159b4398cd
F9419619 Valdeir Ferreira Camargo <valdeir.camargo@bb.com.br>
2023-12-08 13:51:05 -0300

    Projeto 1.0

--- client/js/app/controllers/teste.js
+++ client/js/app/controllers/teste.js
@@ -1,49 +1,10 @@
-class Aluno {
-  constructor(matricula, nome) {
-    this.matricula = matricula;
-    this.nome = nome;
-  }
-}
+let dobro = numeros.map(function(num) {
+  return num * 2;
+});
+let metade = numeros.map(num => num/2);
+let raiz = numeros.map(function(num) {
+  return Math.sqrt(num);
+});
 
-class Prova {
-  constructor(aluno, nota) {
-    this.aluno = aluno;
-    this.nota = nota;
-  }
-}
 
-let avaliacoes = [
-  new Prova(new Aluno(1, "Luana"), 8),
-  new Prova(new Aluno(2, "Cássio"), 7),
-  new Prova(new Aluno(3, "Barney"), 9),
-  new Prova(new Aluno(4, "Bira"), 5),
-];
-
-let aprovados = avaliacoes
-  .filter((prova) => prova.nota >= 7)
-  .map((prova) => prova.aluno.nome);
-
-console.log(aprovados);
-
-/* function fibonacciRecursivo(n) {
-    if (n < 2){
-      return n;
-    } else {
-      return fibonacciRecursivo(n - 1) + fibonacciRecursivo(n - 2);
-    }
-  }
-  
-
-  function fibonacciIterativo(n){
-    let a = 0, b = 1, temp;
-  
-    while (n > 0){
-      temp = a;
-      a = a + b;
-      b = temp;
-      n--;
-    }
-  
-    return a;
-  }
-   */
+console.log(dobro, metade, raiz);
